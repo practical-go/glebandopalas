260eca6621232ccd9f4c6c44a5c868e3f74b63c7
bessarabian <geremin70@gmail.com>
2023-04-08 11:39:27 +0300

    twoPlusTwo function added

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ func greet(name string) {
 	fmt.Printf("Hello %s", name)
 }
 
+func twoPlusTwo() int {
+	return 2 + 2
+}
+
 func main() {
-	greet("Gleb")
+	greet("Gleb!\n")
+
+	fmt.Println("2 + 2: ", twoPlusTwo())
 }
